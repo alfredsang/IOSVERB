4bb3cbcfe35220ad67efcbc12e54109af52a3e4c
shiren1118 <shiren1118@126.com>
2012-04-20 13:19:20 +0800

    init rails3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IOSVERB::Application.config.secret_token = 'a8945821ca80ffcb0b50e013e8e8bcc1c464d15d658e2b85aaa7f2ba66c9fce5336ac9dfaa030ebe8b3b066e30ca99f9909ae20c45873cead5a3c0566988433b'
